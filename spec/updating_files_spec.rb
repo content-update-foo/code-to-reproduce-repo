0db0e3b5a7815f5613be0f3aaf931692cb23d97e
Michael Kay <kay.mikey@gmail.com>
2013-11-04 13:53:53 +0000

    Stopped possibility of thinking its because of concurrency

--- spec/updating_files_spec.rb
+++ spec/updating_files_spec.rb
@@ -24,18 +24,12 @@ describe 'My behaviour' do
   it 'should break on concurrent updates' do
 
     while(true) do
+
       path = "test_data/test_update_#{Time.now}_#{Random.rand}.txt"
 
       response = lets_do_dis { client.create_contents(content_repo, path, 'I am creating', :file => 'spec/fixtures/new_file.txt') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 1', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 2', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 3', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 4', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 5', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 6', response.content.sha, 'Here be even moar damned content') }
-      response = lets_do_dis { client.update_contents(content_repo, path, 'I am updating 7', response.content.sha, 'Here be even moar damned content') }
-      lets_do_dis { client.update_contents(content_repo, path, 'I am updating 8', response.content.sha, 'Here be even moar damned content') }
-
+      sleep(2)
+      lets_do_dis { client.update_contents(content_repo, path, 'I am updating 1', response.content.sha, 'Here be even moar damned content') }
       sleep(2)
     end
 
